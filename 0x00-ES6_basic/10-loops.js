340dbafb470ce014624f64f4b51e4cca9f143d53
philimon-reset <philimonderib2019@gmail.com>
2021-11-22 22:29:09 +0300

    eslint

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
-export default function appendToEachArrayValue(array, appendString) {
-	for (let id of array) {
-	  let value = array.id;
-	  array[id] = appendString + value;
-	}
-      
-	return array;
-      }
+export default function appendToEachArrayValue (array, appendString) {
+  for (const id of array) {
+    const value = array.id;
+    array[id] = appendString + value;
+  }
+
+  return array;
+}
