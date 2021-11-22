7e1794f04f4488004e0921ef9e443df24176744c
philimon-reset <philimonderib2019@gmail.com>
2021-11-22 23:31:51 +0300

    5th question modified

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,3 +1,3 @@
-import appendToEachArrayValue from './10-loops.js';
+import concatArrays from './5-spread-operator.js';
 
-console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
