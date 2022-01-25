3a5d4f63b73559561aadf9e610ce5facc5718970
philimon-reset <philimonderib2019@gmail.com>
2022-01-26 00:44:33 +0300

    http server and csv async comb: fixed end of server #

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -35,6 +35,7 @@ const app = http.createServer((req, res) => {
           newis.forEach((data) => { (final[data[1]] += 1); });
           res.write(`Number of students: ${result.filter((check) => check.length > 3).length}\n`);
           Object.keys(final).forEach((data) => res.write(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}\n`));
+          res.end();
           resolve();
         });
       });
