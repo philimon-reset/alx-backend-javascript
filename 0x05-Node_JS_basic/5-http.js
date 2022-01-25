96adbd46c6322cf4e487c1d65cc6b077e6277f35
philimon-reset <philimonderib2019@gmail.com>
2022-01-26 00:53:02 +0300

    http server and csv async comb: fixed end of server #

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -15,31 +15,26 @@ const app = http.createServer((req, res) => {
     res.end();
   } else if (url === '/students') {
     res.write('This is the list of our students\n');
-    if (fs.existsSync(argv[2])) {
-      return new Promise(() => {
-        fs.readFile(argv[2], 'utf8', (err, data) => {
-          if (err) {
-            throw Error('Cannot load the database');
-          }
-          const result = [];
-          data.split('\n').forEach((data) => {
-            result.push(data.split(','));
-          });
-          result.shift();
-          const newis = [];
-          result.forEach((data) => newis.push([data[0], data[3]]));
-          const fields = new Set();
-          newis.forEach((item) => fields.add(item[1]));
-          const final = {};
-          fields.forEach((data) => { (final[data] = 0); });
-          newis.forEach((data) => { (final[data[1]] += 1); });
-          res.write(`Number of students: ${result.filter((check) => check.length > 3).length}\n`);
-          Object.keys(final).forEach((data) => res.write(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}\n`));
-          res.end();
-        });
+    fs.readFile(argv[2], 'utf8', (err, data) => {
+      if (err) {
+        throw Error('Cannot load the database');
+      }
+      const result = [];
+      data.split('\n').forEach((data) => {
+        result.push(data.split(','));
       });
-    }
-    throw Error('Cannot load the database');
+      result.shift();
+      const newis = [];
+      result.forEach((data) => newis.push([data[0], data[3]]));
+      const fields = new Set();
+      newis.forEach((item) => fields.add(item[1]));
+      const final = {};
+      fields.forEach((data) => { (final[data] = 0); });
+      newis.forEach((data) => { (final[data[1]] += 1); });
+      res.write(`Number of students: ${result.filter((check) => check.length > 3).length}\n`);
+      Object.keys(final).forEach((data) => res.write(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}\n`));
+      res.end();
+    });
   }
 });
 
