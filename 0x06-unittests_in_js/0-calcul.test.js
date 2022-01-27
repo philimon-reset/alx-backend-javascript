c23cb6bac2c1e9382dacc7928ae368e95b0273c0
philimon-reset <philimonderib2019@gmail.com>
2022-01-27 12:43:36 +0300

    test #

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,20 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-  it('checking if numbers round', () => {
+describe('calculateNumber', function() {
+  it('checking if numbers round', function() {
     assert.equal(calculateNumber(1, 3), 4);
   });
-  it('checking if numbers round 2nd round', () => {
+  it('checking if numbers round 2nd round', function() {
     assert.equal(calculateNumber(1, 3.7), 5);
   });
-  it('checking if numbers round 3rd round', () => {
+  it('checking if numbers round 3rd round', function() {
     assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-  it('checking if numbers round 4th round', () => {
+  it('checking if numbers round 4th round', function() {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
-  it('checking negative return', () => {
+  it('checking negative return', function() {
     assert.equal(calculateNumber(-1.3, -3.7), -5);
   });
 });
