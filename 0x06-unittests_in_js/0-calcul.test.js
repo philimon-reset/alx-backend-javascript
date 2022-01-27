21d33272161d0acad510675e04190671688c3e02
philimon-reset <philimonderib2019@gmail.com>
2022-01-27 12:54:38 +0300

    test #

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,15 +2,19 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
-  it('whole number', function() {
-    assert.equal(calculateNumber(1, 4), 5);
+  it('checking if numbers round', function() {
+    assert.equal(calculateNumber(1, 3), 4);
   });
-
-  it('should round up', function() {
-    assert.equal(calculateNumber(1.5, 4.5), 7);
+  it('checking if numbers round 2nd round', function() {
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
-
-  it('should round down', function() {
-    assert.equal(calculateNumber(1.2, 4.2), 5);
+  it('checking if numbers round 3rd round', function() {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it('checking if numbers round 4th round', function() {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it('checking negative return', function() {
+    assert.equal(calculateNumber(-1.3, -3.7), -5);
   });
 });
